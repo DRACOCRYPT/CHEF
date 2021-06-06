cafa95e1fa7943501248eebc6dad1fc5d0f44da5
DRACOCRYPT <77237822+DRACOCRYPT@users.noreply.github.com>
2021-06-06 14:45:15 +0530

    Add files via upload

---
+++ ingredients.js
@@ -0,0 +1,15 @@
+
+      
+
+ 
+
+/*3/4 cup (105 grams) all-purpose flour, we use Gold Medal1/4 cup (30 grams) cornstarch
+1/2 teaspoon baking powder
+1/4 teaspoon baking soda
+1 tablespoon sugar
+1/2 teaspoon kosher salt
+1 cup (235 ml) milk, whole or 2% reduced fat milk are best
+1/3 cup (80 ml) vegetable or lightly flavored oil
+1 large egg
+1 teaspoon vanilla extract
+Butter, syrup and/or berries, for serving*/
---
+++ recipe.js
@@ -0,0 +1,25 @@
+/*Essential Waffle Recipe
+
+Prep time
+
+25 minutes
+Cook time
+
+10 minutes
+
+Total time
+
+35 minutes*/
+/*Whisk the dry ingredients together. 
+That’s the flour, cornstarch, baking powder, baking soda, and a touch of salt.
+Whisk the wet ingredients together in another bowl or do what I do and whisk them right in a measuring jug.
+ Hooray for less dishes!
+Combine the wet and dry ingredients.
+ I whisk until I no longer see streaks of flour. 
+ If there are a few small lumps of flour left, that’s okay.
+Set the batter aside for 10 minutes. 
+In this time, you can preheat your oven (I talk about why below) as well as your waffle iron. 
+The 10 minutes of rest time allows the baking powder to activate.
+Cook the waffles. 
+I use about 1/2 cup of batter for each waffle, but the amount of batter will depend on your waffle iron.
+Transfer cooked waffles to a warm (200°F) oven so they stay warm and crisp.*/ 
